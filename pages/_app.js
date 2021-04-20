8df7f94b97b3b81bbed3f040043b242076e203da
Кирилл Самылин <kirill-itb@yandex.ru>
2021-04-20 12:10:39 +0400

    create project and connect material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Test task</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ src/components/MyWonderfulComponent/MyWonderfulComponent.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { styled } from '@material-ui/core/styles';
+import { Grid, Typography } from "@material-ui/core";
+
+export default function MyWonderfulComponent({ props, children }) {
+  const { id, options, count, color, sms } = props;
+  const [ summ, setSumm ] = useState(count);
+  
+  const Title = styled(Typography)({
+    color: color,
+    fontSize: "18px",
+  });
+
+  useEffect(() => {
+    console.log(sms) // output to the MyWonderfulComponent
+
+    if (id && options?.params?.fields?.isDynamic) {
+      setSumm(summ + 1);
+    }
+  }, []);
+
+  console.log(summ);
+
+  return (
+    <>
+      <Title variant="h1">Hello World!</Title>
+      <Grid>
+        <Grid item xs={12}>{children}</Grid>
+      </Grid>
+    </>
+  );
+}
